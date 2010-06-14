3b7403218594d0ce4f556b90252a321468ec388c
Massimo Santini <santini@dsi.unimi.it>
2010-06-14 18:05:12 +0200

    Fixed null dereferencing due to empty elements (description and title)

--- server/resources/code/mappa.js
+++ server/resources/code/mappa.js
@@ -4,10 +4,19 @@ function init() {
 	for ( var i = 0; i < points.length ; i++ ) disegna( points[ i ] );
 }
 
+function estrai( point, tagname ) {
+	var elem = point.parentNode.getElementsByTagName( tagname );
+	if ( ! elem ) return undefined;
+	var fc = elem[ 0 ].firstChild;
+	if ( ! fc ) return undefined;
+	alert( fc.nodeValue );
+	return fc.nodeValue;
+}
+
 function disegna( point ) {
 	var lat_lng = point.firstChild.firstChild.nodeValue.split( ',' );
-	var title = point.parentNode.getElementsByTagName( 'name' )[ 0 ].firstChild.nodeValue;
-	var description = point.parentNode.getElementsByTagName( 'description' )[ 0 ].firstChild.nodeValue;
+	var title = estrai( point, 'name' );
+	var description = estrai( point, 'description' );
 	var src = '/img/' + parseInt( point.parentNode.attributes.getNamedItem( 'xml:id' ).value.split( '_' )[ 1 ] );
 	marker( new Point( lat_lng[ 0 ], lat_lng[ 1 ] ), title, description, src );
 }
