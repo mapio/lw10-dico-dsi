f53eab8ac04215d181b26ef09d4473533733eec0
Massimo Santini <santini@dsi.unimi.it>
2010-06-15 00:08:47 +0200

    We catch execptions related to SIGUP; logging now goes to server.log file; the WSGIRequestHandler is mixed-in so that it logs instead of printing on stderr

--- server/__main__.py
+++ server/__main__.py
@@ -16,25 +16,38 @@
 # along with lw09-dico-dsi.  If not, see <http://www.gnu.org/licenses/>.
 
 from atexit import register
-from logging import StreamHandler, Formatter, getLogger, DEBUG
+from logging import Formatter, getLogger, DEBUG
+from logging.handlers import RotatingFileHandler
 from os import path
-#from signal import signal, SIGHUP
+try:
+	from signal import signal, SIGHUP
+except ImportError:
+	pass
 from sys import argv
-from wsgiref.simple_server import make_server
+from wsgiref.simple_server import make_server, WSGIRequestHandler
 
-ROOT_LOGGER = getLogger( "server" )
+ROOT_LOGGER = getLogger( 'server' )
 ROOT_LOGGER.setLevel( DEBUG )
-sh = StreamHandler()
-sh.setLevel( DEBUG )
-sh.setFormatter( Formatter( "[%(asctime)s] %(levelname)s - %(name)s: %(message)s","%Y/%b/%d %H:%M:%S" ) )
-ROOT_LOGGER.addHandler( sh )
+lh = RotatingFileHandler( 'server.log', maxBytes = 1 * 1024 * 1024 , backupCount = 2 )
+lh.setLevel( DEBUG )
+lh.setFormatter( Formatter( "[%(asctime)s] %(levelname)s - %(name)s: %(message)s","%Y/%b/%d %H:%M:%S" ) )
+ROOT_LOGGER.addHandler( lh )
+
+class LogMixin:
+	def log_message( self, format, *args ):
+		ROOT_LOGGER.info( 'WSGIRequestHandler: %s - - [%s] %s' % ( self.address_string(), self.log_date_time_string(), format % args ) )
+
+class LogginWSGIRequestHandler( LogMixin, WSGIRequestHandler ): pass
 
 import server
 
 register( server.halt )
-#signal( SIGHUP, lambda signum, frame: server.halt() )
+try:
+	signal( SIGHUP, lambda signum, frame: server.halt() )
+except NameError:
+	pass
 
-simple_server = make_server( 'localhost', 8000, server.application )	
+simple_server = make_server( 'localhost', 8000, server.application, handler_class = LogginWSGIRequestHandler )	
 ROOT_LOGGER.info( 'Serving on http://localhost:8000/' )
 try:
 	while not server.stop: simple_server.handle_request()
