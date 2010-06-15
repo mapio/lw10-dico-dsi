8ebfb5d9bd0e68865d4fd19f7ef9b8c76ba1f647
Massimo Santini <santini@dsi.unimi.it>
2010-06-15 13:40:42 +0200

    Preparing sort lecture

--- server/resources/code/sort-metrics.js
+++ server/resources/code/sort-metrics.js
@@ -1,36 +1,5 @@
 DEBUG = true;
 
-var millis = 0;
-function tstart() {
-	millis = (new Date).getTime();
-}
-
-function tstop() {
-	return (new Date).getTime() - millis;
-}
-
-function a_caso( n, min, max ) {
-	var res = [];
-	for ( var i = 0; i < n; i++ ) res.push( Math.floor( Math.random() * ( max - min ) + min ) );
-	return res;  
-}
-
-function metti( arr, elem ) {
-	arr.push( elem );
-}
-
-function togli( arr, idx ) {
-	if ( idx === undefined )
-		return arr.shift();
-	else 
-	var val = arr.splice( idx, 1 );
-	return val[ 0 ];
-}
-
-function smezza( arr ) {
-	return [ arr.slice( 0, arr.length / 2 ), arr.slice( arr.length / 2, arr.length ) ];
-}
-
 function scambia( arr, i, j ) {
 	if ( i == j ) return;
 	var t = arr[ i ];
